6627d6732f7c1e862ed3fdc383a4e12f3b61dd2f
ing181 <ingcei0123@edu.lerum.se>
2022-02-22 21:33:48 +0100

    Arduino lektion 1

---
+++ Sketch/sketch_feb22a/sketch_feb22a.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600); // Vi skapar kontakt med "serial monitor" på den egna datorn.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("HEJ");
+
+}
