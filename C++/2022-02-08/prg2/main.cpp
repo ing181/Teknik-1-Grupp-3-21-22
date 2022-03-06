f1045ccb9876878136c83f6ad2ff35914ad67d69
ing181 <ingcei0123@edu.lerum.se>
2022-03-06 10:43:56 +0100

    Nya mappnamn

---
+++ C++/2022-02-08/prg2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Detta är en kommentar
+    // cout är skärmen
+    // << Utmatningsoperator
+    // Text som ska skrivas ut har "" runt sig
+    // endl
+    // ; Alltid sist
+    // == jämför
+    while (1 == 1) {
+    cout << "Hello world!" << endl;
+    }
+    cout << 76 << endl;
+    cout << (5.0/3.0) << endl;
+    return 0;
+}
